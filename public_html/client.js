e46693a816fd5b3526b3d931ca92ccd6ca7fb73f
Maria <mpederson913@mcad.edu>
2019-04-02 14:28:18 -0500

    Update client.js

--- public_html/client.js
+++ public_html/client.js
@@ -24,8 +24,8 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+    // cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    // cameraOutput.classList.add("taken");
 
 
     var formData = new FormData();
